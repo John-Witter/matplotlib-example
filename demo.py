ddbbfbb5b7c753473f6406111697736452ac1cf3
John-Witter <mrsolginski@gmail.com>
2021-08-27 09:40:51 -0400

    demo.py

---
+++ demo.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3,4],[1,4,9,16])
+
+plt.ylabel('y numbers')
+plt.xlabel('x numbers')
+
+plt.show()
+
+# ✅ 1st: python3 -m pip install -U matplotlib
+# to run: python3 demo.py
